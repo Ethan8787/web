c05db731b71e3448de6a1b2916444256560fd769
Ethan <ssmoon0912@gmail.com>
2025-12-08 00:00:56 +0800

    Update App.jsx
    Wrong path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./Home.jsx";
 import Answer from "./Answer.jsx";
 import NotFound from "./NotFound.jsx";
-import Matrix from "./MatrixBackground";
+import Matrix from "./components/MatrixBackground";
 import "./App.css"
 
 export default function App() {
