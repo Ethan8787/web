72dd6c3b988bbb041cc9842c86887f0147568c2c
Ethan8787 <157335575+Ethan8787@users.noreply.github.com>
2026-02-16 19:25:11 +0800

    try to fix my icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import avatarImg from '../../assets/avatar/512.png';
+import discordIcon from '../../assets/icons/discord.svg';
+import youtubeIcon from '../../assets/icons/youtube.svg';
+import instagramIcon from '../../assets/icons/instagram.svg';
+import githubIcon from '../../assets/icons/github.svg';
+
 import Background from '../Background/Background.jsx';
 import Navbar from '../Navbar/Navbar.jsx';
 import Skills from '../Skills/Skills.jsx';
@@ -19,7 +26,8 @@ export default function Home() {
                                 <div className="thought-dot dot-1"></div>
                             </div>
                             <div className="thought-dot dot-2"></div>
-                            <img id="avatar" src="/src/assets/avatar/512.png" alt="Avatar" />
+                            {/* 2. 這裡改成變數 avatarImg */}
+                            <img id="avatar" src={avatarImg} alt="Avatar" />
                         </div>
                         <h1>Ethan</h1>
                     </div>
@@ -29,20 +37,21 @@ export default function Home() {
                             你好，我是 Ethan，來自台灣的國三程式設計愛好者。
                         </p>
                         <div className="button-group">
+                            {/* 3. 這裡也都改成變數 */}
                             <a className="btn btn-discord" href="https://discord.gg/AyucpYarpa" target="_blank" rel="noreferrer">
-                                <img src="/src/assets/icons/discord.svg" className="btn-icon" alt="icon" />
+                                <img src={discordIcon} className="btn-icon" alt="icon" />
                                 Discord
                             </a>
                             <a className="btn btn-youtube" href="https://www.youtube.com/channel/UCDiFgkr0I6uAgRlBBPnflNw" target="_blank" rel="noreferrer">
-                                <img src="/src/assets/icons/youtube.svg" className="btn-icon" alt="icon" />
+                                <img src={youtubeIcon} className="btn-icon" alt="icon" />
                                 YouTube
                             </a>
                             <a className="btn btn-instagram" href="https://www.instagram.com/ethantw.dev/" target="_blank" rel="noreferrer">
-                                <img src="/src/assets/icons/instagram.svg" className="btn-icon" alt="icon" />
+                                <img src={instagramIcon} className="btn-icon" alt="icon" />
                                 Instagram
                             </a>
                             <a className="btn btn-github" href="https://github.com/Ethan8787/" target="_blank" rel="noreferrer">
-                                <img src="/src/assets/icons/github.svg" className="btn-icon" alt="icon" />
+                                <img src={githubIcon} className="btn-icon" alt="icon" />
                                 GitHub
                             </a>
                         </div>
