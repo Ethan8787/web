8c0e1e051600acd6a39001d7ca4c00f74a00b07c
Ethan8787 <157335575+Ethan8787@users.noreply.github.com>
2026-03-22 23:33:06 +0800

    added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
     server: {
         host: true,
         port: 5173
-    }, plugins: [react()],
+    },
+    plugins: [react(), Sitemap({ hostname: 'https://ethantw.dev' })],
 })
