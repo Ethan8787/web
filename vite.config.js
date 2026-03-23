40cee5a6a165585154719ad46e6e608936d6e09e
Ethan8787 <157335575+Ethan8787@users.noreply.github.com>
2026-03-24 00:18:58 +0800

    fix attempt #1

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
     server: {
         host: true,
         port: 5173
     },
-    plugins: [react(), Sitemap({ hostname: 'https://ethantw.dev' })],
+    plugins: [
+        react(),
+        Sitemap({
+            hostname: 'https://ethantw.dev',
+            generateRobotsTxt: false
+        })
+    ],
 })
